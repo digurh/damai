36304dff257d3b5c3b88c45c195934794cf52c9c
dylan <dylmyl1029@gmail.com>
2018-07-18 16:49:26 -0400

    write lr_find() and methods associated

--- damai/src/learner.py
+++ damai/src/learner.py
@@ -13,11 +13,17 @@ class Learner:
         else self.opt = opt
 
     def lr_find(self, lr_init=1e-7, lr_end = 10, increase_factor=10):
-        losses = self.train(lr_find=True)
-        learn_rates = [lr_init**i for i in range()]
-        for param_group in self.opt.param_groups:
-            param_group['lr'] = lr
+        losses = self.train(lr_find=(lr_init, lr_end, increase_factor))
+        learn_rates = [lr_init]
 
+        while learn_rates[-1] < lr_end:
+            learn_rates.append(learn_rates[-1]*increase_factor)
+
+        self.plot(losses, learn_rates)
+
+    def lr_find_update(self, i_f):
+        for param_group in self.opt.param_groups:
+            param_group['lr'] *= i_f
 
     def save(self):
         pass
@@ -27,5 +33,12 @@ class Learner:
         pass
 
 
-    def train(self, lr_find=False):
+    def train(self, lr_find=None):
         pass
+
+    def plot(self, *args):
+        fig = plt.figure()
+        plt.plot(np.arange(1, len(args[0])+1), args[0])
+        plt.ylabel('loss')
+        plt.xlabel('learn rate')
+        plt.show()
