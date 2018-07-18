bac13b3d4abae1e0942f364d23a119e588007079
dylan <dylmyl1029@gmail.com>
2018-07-18 18:11:59 -0400

    reorgnaize lr_find()

--- damai/src/learner.py
+++ damai/src/learner.py
@@ -22,25 +22,17 @@ class Learner:
     def lr_find(self, lr_init=1e-7, lr_end = 1, increase_factor=10):
         '''
         Allows optimal learning rate to be found using the method from Cyclical
-        Learning Rates for Training Neural Networks: learnig rate starts very
-        low and slowly raises until loss stops decreasing.
+        Learning Rates for Training Neural Networks: learning rate starts very
+        low and slowly raises until loss stops decreasing. Implementation in
+        LRFinder()
 
         Params: lr_init: starting value from which lr will be increased
                 lr_end: lr will increase until it is >= this value
                 increase_factor: amount by which previous learning rate will be
                                  multiplied each step
         '''
-        losses = self.train(lr_find=(lr_init, lr_end, increase_factor))
-        learn_rates = [lr_init]
+        losses = self.train(sch=LRFinder(lr_init, lr_end, increase_factor))
 
-        while learn_rates[-1] < lr_end:
-            learn_rates.append(learn_rates[-1]*increase_factor)
-
-        self.lr_plot(learn_rates, losses)
-
-    def __lr_find_update(self, i_f):
-        for param_group in self.opt.param_groups:
-            param_group['lr'] *= i_f
 
     def save(self):
         pass
@@ -50,13 +42,6 @@ class Learner:
         pass
 
 
-    def train(self, lr_find=None):
-        pass
+    def train(self, sch=None):
 
-
-    def __lr_plot(self, *args):
-        fig = plt.figure()
-        plt.plot(args[0], args[1])
-        plt.ylabel('loss')
-        plt.xlabel('learn rate')
-        plt.show()
+        for i, (X, y) in enumerate(self.data.batch_load()):
